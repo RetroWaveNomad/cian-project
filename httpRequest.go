6a7bd5600d83d9f1db167cac3febb2558d146037
Alexandr Saderdinov <51410298+RetroWaveNomad@users.noreply.github.com>
2020-07-08 20:55:47 +0300

    Update httpRequest.go

--- httpRequest.go
+++ httpRequest.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"regexp"
 )
 
 func main() {
@@ -16,19 +13,4 @@ func main() {
 	}
 	defer response.Body.Close()
 
-	// Read response data in to memory
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal("Error reading HTTP body. ", err)
-	}
-
-	// Create a regular expression to find appartments for rent
-	re := regexp.MustCompile("₽/мес.")
-	words := re.FindAllString(string(body), -1)
-	if words == nil {
-		fmt.Println("No matches.")
-	} else {
-		//Just an example, nod done yet
-		fmt.Println(" адрес: Москва, улица Пушкина, дом 10; формат: двушка; цена: пять десят тысяч рублей в месяц")
-	}
 }
